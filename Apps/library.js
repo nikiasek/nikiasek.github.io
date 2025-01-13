e864a44ca30c3413ed10c5f403b248530e38fcf5
nikiasek <nikiasek50@gmail.com>
2025-01-13 09:17:59 +0100

    asd

---
+++ Apps/library.js
@@ -0,0 +1,20 @@
+const testdict = {
+    test1 : {
+        name : "Test1",
+        link: "https://www.geeksforgeeks.org/how-to-create-dictionary-and-add-key-value-pairs-dynamically/",
+        flag : ["javascript", "html"]
+    },
+    test2 : {
+        name : "Test2",
+        link: "https://www.geeksforgeeks.org/how-to-create-dictionary-and-add-key-value-pairs-dynamically/",
+        flag : ["python", "html"]
+    },
+    test3 : {
+        name : "Test3",
+        link: "https://www.geeksforgeeks.org/how-to-create-dictionary-and-add-key-value-pairs-dynamically/",
+        flag : ["django", "react"]
+    }
+}
+const div = document.getElementById("items")
+
+div.innerHTML = "<h1>test</h1>"
