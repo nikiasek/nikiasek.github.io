50ccbc24713cbb6c8302321582d3aab7039448af
nikiasek <nikiasek50@gmail.com>
2025-01-27 09:21:53 +0100

    asd

--- Apps/translation.js
+++ Apps/translation.js
@@ -25,18 +25,29 @@ document.addEventListener("DOMContentLoaded", () => {
         document.querySelector(".library2").innerText = translations.header.library;
         document.querySelector(".intro").innerText = translations.main.intro;
         document.querySelector(".description").innerText = translations.main.description;
-        document.querySelector(".description2").innerText = translations.main.description2;
+        document.querySelector(".description2").innerHTML = `<span class="gradient-text">${translations.main.description2}</span>`
         document.querySelector(".vision").innerText = translations.main.vision;
         document.querySelector(".vision2").innerText = translations.main.vision;
-        document.querySelector(".focus").innerHTML = `${translations.main.focus} <span class="gradient-text">${translations.main.focus2}</span>`
-        document.querySelector(".programmer").innerText = translations.main.programmer
-        document.querySelector(".programmer2").innerText = translations.main.programmer2
+        document.querySelector(".focus").innerHTML =    `${translations.main.focus} <span class="gradient-text">${translations.main.focus2}</span>`;
+        document.querySelector(".programmer").innerText = translations.main.programmer;
+        document.querySelector(".programmer2").innerText = translations.main.programmer2;
         document.querySelector(".projects").innerText = translations.main.projects;
-        document.querySelector(".project_description").innerText = translations.main.project_description
-    
-    
+        document.querySelector(".project_description").innerText = translations.main.project_description;
+        document.querySelector(".tools").innerText = translations.main.tools
+        document.querySelector(".tools_description").innerText = translations.main.tools_description
+        document.querySelector(".programming_lang").innerText = translations.main.programming_lang
+        document.querySelector(".frameworks").innerText = translations.main.frameworks
+        document.querySelector(".libraries").innerText = translations.main.libraries
+        document.querySelector(".get_in_touch").innerText = translations.footer.get_in_touch
+        document.querySelector(".connect").innerText = translations.footer.connect
+        document.querySelector(".portfolio").innerText = translations.footer.portfolio
+
     };  
 
+    const dropdownEvent = () => {
+        document.querySelector(".dropdown").dropdown
+    }
+
     // Load default language
-    loadTranslations("cz");
+    loadTranslations("es");
 });
